0c250bfd85cf2f1e5190bccd98f6d2ef7bf258c2
eugeniarossi <eugenia.rossi@outlook.com>
2023-11-01 17:04:34 +0100

    feat: getMeme function

--- src/components/BattleComponent.vue
+++ src/components/BattleComponent.vue
@@ -1,4 +1,6 @@
 <script>
+// importo axios
+import axios from 'axios';
 
 export default {
     name: 'BattleComponent',
@@ -6,7 +8,25 @@ export default {
     },
     data() {
         return {
-            
+            memes: [], // memes attualmente nella battle
+        }
+    },
+    methods: {
+        async getMeme() {
+            if (this.memes.length < 2) {
+                try {
+                    const newMeme = await axios.get('https://meme-api.com/gimme').then(response => response.data);
+                    // salvo i dati della chiamata API nella variabile newMeme
+
+                    // aggiungo la proprietà score e assegno 0
+                    newMeme.score = 0;
+
+                    this.memes.push(newMeme); // pusho newMeme nell'array memes
+                } catch (error) {
+                    console.error('Errore:', error);
+                    throw error;
+                }
+            } /////// posso fare una chiamata post per salvare dati nel db qui ///
         }
     }
 }
