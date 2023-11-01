afb38ba3a8324f8218f6f1c164e60b2cffecdec1
eugeniarossi <eugenia.rossi@outlook.com>
2023-11-01 17:00:40 +0100

    import and dependencies: axios - bootstrap

--- src/main.js
+++ src/main.js
@@ -1,5 +1,9 @@
 import { createApp } from 'vue'
+// importo bootstrap
+import 'bootstrap/dist/css/bootstrap.css'
 import './assets/main.scss'
 import App from './App.vue'
+
+
 
 createApp(App).mount('#app')
