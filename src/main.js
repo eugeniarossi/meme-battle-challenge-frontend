3ab247b9d5d9e55238de5940d9d375f12bfcc380
eugeniarossi <eugenia.rossi@outlook.com>
2023-11-02 08:11:21 +0100

    import jquery & popper

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
 import { createApp } from 'vue'
+
 // importo bootstrap
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.bundle.js'
+
+// importo jquery
+import $ from 'jquery'
+
 import './assets/main.scss'
 import App from './App.vue'
-
-
 
 createApp(App).mount('#app')
