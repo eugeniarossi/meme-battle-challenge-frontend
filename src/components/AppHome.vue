1cf910398c246973328da1946c0097aa9e9963f3
eugeniarossi <eugenia.rossi@outlook.com>
2023-11-02 13:07:44 +0100

    fix: layout button

--- src/components/AppHome.vue
+++ src/components/AppHome.vue
@@ -35,20 +35,30 @@ export default {
                 console.error('Errore durante il recupero dei dati:', error);
             }
         },
+        // funzione per aggiungere classe css ad un elemento
+        addClass(element, className) {
+            element.classList.add(className);
+        },
+        // funzione per rimuovere classe css ad un elemento
+        removeClass(element, className) {
+            element.classList.remove(className);
+        },
+        // funzione per cambiare layout
         changeLayout() {
+            //al click layoutReversed diventa true
             this.layoutReversed = !this.layoutReversed;
 
             const rowLayout = document.getElementById('row-layout'); // seleziona l'elemento row-layout
-            const buttonLayout = document.getElementById('layout-button'); // seleziona il button layout
+            const buttonLayout = document.getElementById('layout-buttons'); // seleziona il button layout
 
-            if (this.layoutReversed) {
-                rowLayout.classList.add('flex-xl-row-reverse');
-                buttonLayout.classList.remove('layout-button-right');
-                buttonLayout.classList.add('layout-button-left');
+            if (this.layoutReversed) { // inverto le classi
+                this.addClass(rowLayout, 'flex-xl-row-reverse');
+                this.removeClass(buttonLayout, 'layout-buttons-right');
+                this.addClass(buttonLayout, 'layout-buttons-left');
             } else {
-                rowLayout.classList.remove('flex-xl-row-reverse');
-                buttonLayout.classList.remove('layout-button-left');
-                buttonLayout.classList.add('layout-button-right');
+                this.removeClass(rowLayout, 'flex-xl-row-reverse');
+                this.removeClass(buttonLayout, 'layout-buttons-left');
+                this.addClass(buttonLayout, 'layout-buttons-right');
             }
         }
     },
@@ -61,9 +71,11 @@ export default {
 
 <template>
     <!-- change layout button -->
-    <button id="layout-button" class="layout-button-right btn d-none d-xl-block" @click="changeLayout()">
-        <small><i class="fa-solid fa-right-left"></i> Change layout</small>
-    </button>
+    <div id="layout-buttons" class="d-flex layout-buttons-right">
+        <button id="layout-position-button" class="layout-button mx-1 btn d-none d-xl-block" @click="changeLayout()">
+            <i class="fa-solid fa-right-left"></i> Change layout
+        </button>
+    </div>
     <!-- /change layout button -->
     <!-- container -->
     <div class="container-fluid p-0 m-0">
@@ -82,25 +94,29 @@ export default {
 </template>
 
 <style scoped lang="scss">
-#layout-button {
+#layout-buttons {
     position: absolute;
     top: .625rem;
-    background-color: #6794d7;
 }
 
-#layout-button:hover {
+.layout-button {
+    background-color: #6794d7;
+    font-size: .8125rem;
+}
+
+.layout-button:hover {
     color: white;
 }
 
-#layout-button:active {
+.layout-button:active {
     background-color: transparent;
 }
 
-.layout-button-right {
+.layout-buttons-right {
     right: .625rem;
 }
 
-.layout-button-left {
+.layout-buttons-left {
     left: .625rem;
 }
 
--- src/store.js
+++ src/store.js
@@ -1,7 +1,7 @@
 import { reactive } from "vue";
 
 const store = reactive({
-    apiDbUrl: 'http://127.0.0.1:8000/api/memes', // url del database
+    apiDbUrl: 'http://127.0.0.1:8000/api/memes', // url api database
     apiMemes: 'https://meme-api.com/gimme' // url api meme
 })
 
