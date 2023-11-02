d3981cf20b23b720ed22a4a714e75298240035f9
eugeniarossi <eugenia.rossi@outlook.com>
2023-11-02 08:13:27 +0100

    feat: LeaderboardItem component

--- src/components/LeaderboardComponent.vue
+++ src/components/LeaderboardComponent.vue
@@ -1,34 +1,58 @@
 <script>
+// importo LeaderboardItem
+import LeaderboardItem from './LeaderboardItem.vue';
+
 export default {
     name: 'LeaderboardComponent',
     components: {
+        LeaderboardItem
     },
-    props: {
+    props: { // dichiaro le props
         scoreOrderedMemes: Array,
-    },
-    methods: {
-        parsePreview(meme) {
-            const parsePreview = JSON.parse(meme.preview);
-            return parsePreview;
-        }
     }
 }
 </script>
 
 <template>
-    <div class="card" style="width: 18rem;">
-        <h4>Leaderboard</h4>
-        <ul class="list-group list-group-flush">
-            <li class="list-group-item d-flex" v-for="meme in scoreOrderedMemes">
-                <img :src="parsePreview(meme)[0]" :alt="meme.title">
-                <div class="infos">
-                    <p>Id {{ meme.id }}</p>
-                    <p>Title {{ meme.title }}</p>
-                    <p>Score {{ meme.score }}</p>
+    <div class="leaderboard-col col-auto">
+        <button class="btn btn-primary d-xl-none px-3" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasExample">Leaderboard</button>
+
+        <div class="offcanvas offcanvas-start" tabindex="-1" id="offcanvasExample">
+
+            <div class="card" style="width: 100;">
+                <div class="offcanvas-header d-flex align-items-center justify-content-between px-3">
+                    <h4 class="text-center py-2">Leaderboard</h4>
+                    <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
                 </div>
-            </li>
-        </ul>
+                <ul class="offcanvas-body list-group list-group-flush">
+                    <li class="list-group-item d-flex p-3" v-for="meme in scoreOrderedMemes">
+                        <LeaderboardItem :meme="meme" />
+                    </li>
+                </ul>
+            </div>
+
+        </div>
+
+        <div class="card d-none d-xl-block" style="width: 18rem;">
+            <h4 class="text-center py-2">Leaderboard</h4>
+            <ul class="list-group list-group-flush">
+                <li class="list-group-item d-flex p-3" v-for="meme in scoreOrderedMemes">
+                    <LeaderboardItem :meme="meme" />
+                </li>
+            </ul>
+        </div>
     </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss" scoped>
+/*
+.leaderboard-col {
+    position: absolute;
+    top: 0;
+    left: 0;
+}*/
+.card {
+    max-height: 100vh;
+    overflow-y: scroll;
+}
+</style>
---
+++ src/components/LeaderboardItem.vue
@@ -0,0 +1,27 @@
+<script>
+
+export default {
+    name: 'LeaderboardItem',
+    props: { // dichiaro le props
+        meme: Object
+    },
+    methods: {
+        // funzione per trasformare in array il contenuto di preview
+        parsePreview(meme) {
+            const parsePreview = JSON.parse(meme.preview);
+            return parsePreview;
+        }
+    }
+}
+</script>
+
+<template>
+    <img :src="parsePreview(meme)[0]" :alt="meme.title">
+    <div class="infos px-4">
+        <p>Id {{ meme.id }}</p>
+        <p>Title {{ meme.title }}</p>
+        <p>Score {{ meme.score }}</p>
+    </div>
+</template>
+
+<style lang="scss" scoped></style>
